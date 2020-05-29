f8a647ebcdfef8a1566eac00e9bb6010e367e8fb
Thuc <huynhthuchct@gmail.com>
2020-05-30 01:56:48 +0700

    api up link images

--- server/service/tool.service.js
+++ server/service/tool.service.js
@@ -7,6 +7,37 @@ path = require('path')
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const User = db.User;
+var multer  = require('multer')
+var dirmain = path.join(__dirname, '../');
+var fs = require('fs');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, dirmain+'/public/store/');
+    },
+    filename: async function (req, file, cb) {
+      let file_new_name = file.fieldname + '-' + Date.now()+'-'+file.filename+'.jpg';
+      cb(null, file_new_name);
+      let user = await User.findOne({_id:req.user.sub});
+      if(user){
+          let array_images = user.list_images;
+          try{
+            array_images=[].concat(array_images,[file_new_name])
+            user.list_images=array_images;
+            
+            fs.appendFile(dirmain+"/cache/file"+'-'+req.user.sub, file_new_name+'\n', function (err) {
+                if (err) throw err;
+                console.log(file_new_name);
+              });
+          }catch(err){
+            throw err;
+          }
+         
+      }
+    }
+  })
+var upload = multer({ storage:storage}).array("images")
+
 
 module.exports = {
     upFile,
@@ -34,19 +65,32 @@ async function upFile(req,res){
     return old_list_array;
 }
 async function updateFileExpress(req,res){
-    console.log(req);
-    if(!req.files || Object.keys(req.files).length==0){
-        return res.status(400).send('No files were uploaded.');
-    }
-    
-    let sampleFile = req.files.sampleFile;
-    let dirmain = path.join(__dirname, '../');
-
-    sampleFile.mv(dirmain+"/public/store/"+sampleFile.name, function(err) {
-        if (err)
-          return res.status(500).send(err);
-        res.send('File uploaded!');
-      });
+    console.log(req.body);
+    // if(!req.files || Object.keys(req.files).length==0){
+    //     return res.status(400).send('No files were uploaded.');
+    // }
+    upload(req,res,async function(agr,err){
+        console.log(agr);
+        fs.readFile(dirmain+"/cache/file-"+req.user.sub, 'utf8',async function (err,data) {
+            if (err) {
+              return console.log(err);
+            }
+            var user = await User.findById(req.user.sub);
+            user.list_images=[].concat(data.split('\n').filter((el)=>el!='').map((el)=>process.env.HOST+"/static/store/"+el),user.list_images);
+            // user.list_images=[];
+            await user.save();
+            fs.unlink(dirmain+"/cache/file-"+req.user.sub, function (err) {
+                if (err) throw err;
+                console.log('File deleted!');
+              });
+            console.log("save");
+          });
+        if (err instanceof multer.MulterError) {
+           throw err;
+          } else if (err) {
+            throw err;
+        }
+    });
 }
 async function updateImageForUser(req,res){
     var user = await User.findOne({_id:req.user.sub});
